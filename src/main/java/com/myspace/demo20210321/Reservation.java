e421112fde7768df74c4f01cb3c135ba051edc58
krisv <>
2021-03-21 15:03:16 +0100

    {/src/main/java/com/myspace/demo20210321/Reservation.java}

--- src/main/java/com/myspace/demo20210321/Reservation.java
+++ src/main/java/com/myspace/demo20210321/Reservation.java
@@ -6,12 +6,45 @@ package com.myspace.demo20210321;
 
 public class Reservation implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Reservation() {
-    }
+	private java.time.LocalDate checkin;
+	private java.time.LocalDate checkout;
+	private java.util.List<java.lang.String> guests;
 
+	public Reservation() {
+	}
 
+	public java.time.LocalDate getCheckin() {
+		return this.checkin;
+	}
 
+	public void setCheckin(java.time.LocalDate checkin) {
+		this.checkin = checkin;
+	}
+
+	public java.time.LocalDate getCheckout() {
+		return this.checkout;
+	}
+
+	public void setCheckout(java.time.LocalDate checkout) {
+		this.checkout = checkout;
+	}
+
+	public java.util.List<java.lang.String> getGuests() {
+		return this.guests;
+	}
+
+	public void setGuests(java.util.List<java.lang.String> guests) {
+		this.guests = guests;
+	}
+
+	public Reservation(java.time.LocalDate checkin,
+			java.time.LocalDate checkout,
+			java.util.List<java.lang.String> guests) {
+		this.checkin = checkin;
+		this.checkout = checkout;
+		this.guests = guests;
+	}
 
 }
